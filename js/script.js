846ffa8e37d64f2e21b222ee0eb2a0a97fbc5336
francesco <f.iacono89@gmail.com>
2020-11-10 16:14:35 +0100

    primo commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var nome = prompt("Your nome");
+// console.log(nome);
+var sournome = prompt("Your sournome");
+// console.log(sournome);
+var favoriteColor = prompt("Your favorite color");
+// console.log(favoriteColor);
+
+document.getElementById('password').innerHTML = nome + sournome + favoriteColor + 19;
